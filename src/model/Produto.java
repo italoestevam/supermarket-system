b95cd1c7835355209fcd912ef0b2fad66f7e6656
italoestevam <italoestevam21@gmail.com>
2026-08-19 20:29:18 -0300

    Implement model-produto

---
+++ src/model/Produto.java
@@ -0,0 +1,46 @@
+package model;
+public class Produto {
+
+    //Atributos
+    private long id ;
+    private String nome;
+    private double preco;
+    private int estoque;
+
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public double getPreco() {
+        return preco;
+    }
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+    public int getEstoque() {
+        return estoque;
+    }
+    public void setEstoque(int estoque) {
+        this.estoque = estoque;
+    }
+
+    //Construtores
+    public Produto() {
+
+    }
+    public Produto(long id, String nome, double preco, int estoque) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+        this.estoque = estoque;
+    }
+}
